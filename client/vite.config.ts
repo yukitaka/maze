406f106871b6c8fa9328d6d20176b61f70b9712b
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-24 22:42:11 +0900

    feat: create client

---
+++ client/vite.config.ts
@@ -0,0 +1,15 @@
+import solid from "solid-start/vite";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [solid()],
+  test: {
+    deps: {
+      registerNodeLoader: true,
+    },
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["node_modules/@testing-library/jest-dom/extend-expect"],
+    transformMode: { web: [/\.[jt]sx?$/] },
+  },
+});
