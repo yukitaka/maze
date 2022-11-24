f21b621fae1877162c75706b735b9f567da6fc95
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-24 23:55:36 +0900

    feat: add style

--- client/src/components/MazeCreator.tsx
+++ client/src/components/MazeCreator.tsx
@@ -1,5 +1,6 @@
 import { FormError } from "solid-start/data";
 import { createServerAction$ } from "solid-start/server";
+import "./MazeCreator.css";
 
 export default function MazeCreator() {
   const [creator, { Form }] = createServerAction$(async (form: FormData) => {
@@ -8,8 +9,10 @@ export default function MazeCreator() {
   });
   return (
     <Form>
-      <input type="number" name="width" min="5" />
-      <input type="number" name="height" min="5" />
+      <label for="width-input">Width: </label>
+      <input type="number" name="width" min="5" /><br />
+      <label for="width-input">Height: </label>
+      <input type="number" name="height" min="5" /><br />
       <button class="creator">
         Create
       </button>
