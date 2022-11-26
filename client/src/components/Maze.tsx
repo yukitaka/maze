06924fb633805fe71492ca9e68ac497cd94efc60
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-26 23:42:18 +0900

    feat: implement each line to view

--- client/src/components/Maze.tsx
+++ client/src/components/Maze.tsx
@@ -3,9 +3,9 @@ import { createSignal, For } from "solid-js";
 export default function Maze(props) {
   return (
     <>
-      <For each={props.maze}>
-        {(item) => item == 1 ? "■" : "　"}
-      </For>
+      <For each={props.maze}>{
+        items => <div>{items.map((item) => item == 1 ? 'o' : ' ')}</div>
+      }</For>
     </>
   )
 }
