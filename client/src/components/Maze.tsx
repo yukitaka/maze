f5b6e282e3d9bd10e57b32e1d271470109e315e4
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-26 23:51:09 +0900

    feat: modify maze width from props

--- client/src/components/Maze.tsx
+++ client/src/components/Maze.tsx
@@ -2,8 +2,9 @@ import { createSignal, For } from "solid-js";
 import "./Maze.css";
 
 export default function Maze(props) {
+
   return (
-    <div class="maze">
+    <div class="maze" style={{ width: `${props.maze.length}em` }}>
       <For each={props.maze}>{
         items => <div>{items.map((item) => item == 1 ? '■' : '□')}</div>
       }</For>
