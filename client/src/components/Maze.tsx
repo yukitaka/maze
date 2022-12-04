9e2babed00cb8e864efee01c7465cedd2f5f3d83
Takayuki Sato <s.yukkiy@gmail.com>
2022-12-05 00:55:53 +0900

    feat: divide components to a wall and a road

--- client/src/components/Maze.tsx
+++ client/src/components/Maze.tsx
@@ -1,12 +1,14 @@
 import { createSignal, For } from "solid-js";
 import "./Maze.css";
+import Wall from "~/components/Wall";
+import Road from "~/components/Road";
 
 export default function Maze(props) {
 
   return (
     <div class="maze" style={{ width: `${props.maze.length}em` }}>
       <For each={props.maze}>{
-        items => <div>{items.map((item) => item == 1 ? '■' : '□')}</div>
+        items => <div>{items.map((item) => item == 1 ? (<Wall />) : (<Road />))}</div>
       }</For>
     </div>
   )
---
+++ client/src/components/Road.tsx
@@ -0,0 +1,5 @@
+import "./Road.css";
+
+export default function Road() {
+  return (<span class="road">□</span>);
+}
